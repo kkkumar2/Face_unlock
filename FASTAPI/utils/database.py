0867ba8ea4cd54c1899d66a3770b9c7a6aa320e5
sandeep <jena.sandp@gmail.com>
2022-05-20 18:49:44 +0530

    database updated

--- FASTAPI/utils/database.py
+++ FASTAPI/utils/database.py
@@ -1,6 +1,14 @@
 
-# from pymongo import MongoClient
+from pymongo import MongoClient
+import pymongo
 
-# client = MongoClient("mongodb+srv://prince:<password>@cluster0.xfinu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-# store = client.storehash
-# db = store['All_User_Data']
+
+try:
+    client = MongoClient("mongodb://sandeepjena:Siku8339@cluster0-shard-00-00.tetcj.mongodb.net:27017,cluster0-shard-00-01.tetcj.mongodb.net:27017,cluster0-shard-00-02.tetcj.mongodb.net:27017/?ssl=true&replicaSet=atlas-tzvisj-shard-0&authSource=admin&retryWrites=true&w=majority")
+except pymongo.errors.ConfigurationError as e :
+    raise e
+except pymongo.errors.ConnectionFailure as f:
+    raise f
+    
+store = client.storehash
+db = store['All_User_Data']
