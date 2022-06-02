e7bbd1c625fd0b07739ceee8d3caad47bc7c2bb8
MK <mk23.mohankumar@gmail.com>
2022-06-02 15:06:57 +0530

    Emotion added

--- STREAMLIT/FACE_VERIFICATION/face_detector.py
+++ STREAMLIT/FACE_VERIFICATION/face_detector.py
@@ -2,6 +2,14 @@ import streamlit as st
 import cv2 
 import face_recognition
 import os
+
+##new
+
+# from deepface import DeepFace
+from fer import FER
+
+##new
+
 class FaceDetector:
     def __init__(self):
         # self.WINDOW = st.image([])
@@ -14,8 +22,16 @@ class FaceDetector:
         flag = False
         for (y1, x2, y2, x1) in boxes:
             # cropped_face = RGB[y1:y2, x1:x2]
+            # analyze = DeepFace.analyze(img,actions=['emotions'])
+            # emotion = analyze["dominanmt_emotion"]
+            emo_detector = FER(mtcnn=True)
+            captured_emotions = emo_detector.detect_emotions(RGB)
+            dominant_emotion, emotion_score = emo_detector.top_emotion(RGB)
+
             flag = True
             cv2.rectangle(RGB, (x1, y1), (x2, y2), (255, 0, 0), 2)
+            label_position = (x1-3,y1-3)
+            cv2.putText(RGB,dominant_emotion,label_position,cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),3)
             # face = cv2.resize(RGB, (200, 200))
             WINDOW.image(RGB)
         if flag:
